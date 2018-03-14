ae583d9e83d7277133283c410e1143e335b40f80
gregbee2 <gregory.beirens@gmail.com>
2018-03-14 09:39:57 +0100

    added readme

--- dist/xAssist-csv.js
+++ dist/xAssist-csv.js
@@ -1,6 +1,6 @@
 // https://github.com/GregBee2/xassist-csv#readme Version 0.0.1.
 // Copyright 2018 undefined.
-// Created on Wed, 14 Mar 2018 08:39:02 GMT.
+// Created on Wed, 14 Mar 2018 08:39:55 GMT.
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('@xassist/xassist-object'), require('@xassist/xassist-array')) :
 	typeof define === 'function' && define.amd ? define(['exports', '@xassist/xassist-object', '@xassist/xassist-array'], factory) :
