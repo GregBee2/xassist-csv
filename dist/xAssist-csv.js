4c0a808154509d9100e23f00e670152afcec048c
gregbee2 <gregory.beirens@gmail.com>
2018-03-15 09:30:50 +0100

    publish latest version

--- dist/xAssist-csv.js
+++ dist/xAssist-csv.js
@@ -1,8 +1,8 @@
 /**
 * @preserve
-* https://github.com/GregBee2/xassist-csv#readme Version 1.0.3.
+* https://github.com/GregBee2/xassist-csv#readme Version 1.0.4.
 *  Copyright 2018 undefined.
-*  Created on Wed, 14 Mar 2018 12:58:22 GMT.
+*  Created on Thu, 15 Mar 2018 08:30:46 GMT.
 */
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('@xassist/xassist-object'), require('@xassist/xassist-array')) :
